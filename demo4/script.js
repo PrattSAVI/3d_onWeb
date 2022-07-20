00f885040f1a0ab86a61091b30acb0ac0be9b30b
Pratt SAVI <58483678+PrattSAVI@users.noreply.github.com>
2022-07-20 08:09:16 -0400

    Simplify and replace data source

--- demo4/script.js
+++ demo4/script.js
@@ -2,106 +2,62 @@ import {
     Scene,
     PerspectiveCamera,
     WebGLRenderer,
-    sRGBEncoding,
     Clock,
-    Vector3,
-    Quaternion,
     Matrix4,
-    Euler
-} from 'three'
+    Euler,
+    Color,
+    AmbientLight
+} from 'three';
+
 import { Loader3DTiles } from 'three-loader-3dtiles'
-import { CameraRig, StoryPointsControls } from 'three-story-controls'
-import cameraData from './camera-data.js'
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
 
 // Set up the three.js scene
 const canvasParent = document.querySelector('.canvas-parent')
 const captionElement = document.querySelector('.caption')
+
 const scene = new Scene()
-const camera = new PerspectiveCamera(60, canvasParent.clientWidth / canvasParent.clientHeight, 0.001, 1000)
-const renderer = new WebGLRenderer()
+scene.background = new Color(0xff0000);
+
+const camera = new PerspectiveCamera(60, canvasParent.clientWidth / canvasParent.clientHeight, 0.001, 100000)
+const renderer = new WebGLRenderer({ antialias: true })
+
 renderer.setSize(canvasParent.clientWidth, canvasParent.clientHeight)
-renderer.outputEncoding = sRGBEncoding
+    //renderer.outputEncoding = sRGBEncoding
 canvasParent.appendChild(renderer.domElement)
+camera.position.set(0, 0, 200);
+
+
+// controls - Orbit
+let controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+controls.dampingFactor = 0.05;
 
 const clock = new Clock()
-const rig = new CameraRig(camera, scene)
 let tilesRuntime
 
-// Points of Interest (POIs) captions
-const captions = [
-    'This is amazing!!!',
-    'Exercitation consectetur est anim excepteur dolor. Sit qui deserunt proident excepteur aliquip sunt esse ullamco ex ipsum Lorem minim consectetur.',
-    'Dolor nostrud amet ad irure qui ea. Fugiat cupidatat anim aliqua duis cupidatat cupidatat nisi. Occaecat eu magna id aliqua.',
-    'Amet cupidatat officia enim eu sunt pariatur tempor consequat culpa ullamco proident. In voluptate ad sit enim ipsum dolore. ',
-    'Aliquip laboris ea non ex incididunt tempor.',
-    'Ad incididunt esse duis occaecat proident nostrud. Minim non est proident consequat excepteur nulla ad esse exercitation amet id.',
-    'Dolor consectetur sint excepteur officia quis do mollit commodo deserunt dolor adipisicing. Minim adipisicing aliqua ipsum voluptate elit amet do do ullamco Lorem Lorem consectetur.',
-    'Amet cillum do laboris nulla. Amet id elit et labore cillum elit sit deserunt do reprehenderit occaecat.',
-    'Amet cupidatat officia enim eu sunt pariatur tempor consequat culpa ullamco proident. In voluptate ad sit enim ipsum dolore. ',
-    'Aliquip laboris ea non ex incididunt tempor.',
-    'Ad incididunt esse duis occaecat proident nostrud. Minim non est proident consequat excepteur nulla ad esse exercitation amet id.',
-    'Dolor consectetur sint excepteur officia quis do mollit commodo deserunt dolor adipisicing. Minim adipisicing aliqua ipsum voluptate elit amet do do ullamco Lorem Lorem consectetur.',
-]
-
-// Create POIs with data exported from the CameraHelper tool 
-// (see here for more: https://nytimes.github.io/three-story-controls/#camera-helper)
-// Note: Any method of listing camera position and quaternion will work for StoryPointControls
-const pois = cameraData.pois.map((poi, i) => {
-    return {
-        position: new Vector3(...poi.position),
-        quaternion: new Quaternion(...poi.quaternion),
-        duration: poi.duration,
-        ease: 'power1.in',
-    }
-})
-
-console.log('These are the POIs')
-console.log(pois);
-// Initialize StoryPointControls with poi data
-const controls = new StoryPointsControls(rig, pois)
-document.querySelector('.next').addEventListener('click', () => controls.nextPOI())
-document.querySelector('.prev').addEventListener('click', () => controls.prevPOI())
-    // Update captions when the POI changes
-controls.addEventListener('update', (e) => {
-    if (e.progress === 0) {
-        captionElement.classList.remove('visible')
-    } else if (e.progress === 1) {
-        captionElement.classList.add('visible')
-        captionElement.innerHTML = `
-      <p><span> ${e.upcomingIndex + 1}/${captions.length} </span> ${captions[e.upcomingIndex]}</p>
-      `
-    }
-})
-captionElement.addEventListener('click', () => captionElement.classList.toggle('hidden'))
-
-// Load the tileset
-// See here for more options: https://github.com/nytimes/three-loader-3dtiles/blob/dev/docs/three-loader-3dtiles.loaderprops.md
-
-/* Links and example from Cesium Tile.
-const tileset = viewer.scene.primitives.add(
-  new Cesium.Cesium3DTileset({
-    url: Cesium.IonResource.fromAssetId(1213511),
-  })
-);
-*/
+// Import 3D Tiles
+let my_url = "assets/tileset_2/tileset.json";
 
 Loader3DTiles.load({
-    url: 'https://storage.googleapis.com/rd-big-files/tilesets/ONA360/TILESET/0731_FREEMAN_ALLEY_10M_A_36x8K__10K-PN_50P_DB/tileset_tileset.json',
+    url: my_url,
     renderer: renderer,
     options: {
-        dracoDecoderPath: 'https://unpkg.com/three@0.129.0/examples/js/libs/draco',
-        basisTranscoderPath: 'https://unpkg.com/three@0.129.0/examples/js/libs/basis',
         initialTransform: new Matrix4()
-            .makeRotationFromEuler(new Euler(-Math.PI / 2, 0, 0))
+            .makeRotationFromEuler(new Euler(0, Math.PI / 2, 0))
             .setPosition(0, 0, 0)
     }
 }).then(result => {
     const { model, runtime } = result
     tilesRuntime = runtime
+    console.log(model['children'])
     scene.add(model)
-    controls.enable()
-    controls.goToPOI(0)
 })
+
+// -------- Lights
+var light = new AmbientLight(0xffffff);
+light.intensity = 1;
+scene.add(light);
 
 // Render loop
 function render(t) {
@@ -110,7 +66,7 @@ function render(t) {
     if (tilesRuntime) {
         tilesRuntime.update(dt, renderer, camera)
     }
-    controls.update(t)
+    //controls.update(t)
     renderer.render(scene, camera)
 }
 
